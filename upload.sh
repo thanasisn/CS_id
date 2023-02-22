056608cdd870960a24d73859f77a9dae1990b8f2
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-02-22 10:13:14 +0200

    Commit 2023-02-22 10:13

--- upload.sh
+++ upload.sh
@@ -3,21 +3,17 @@
 
 #### enter description here
 
-echo "i am $0 who the hell are you"
-TIC=$(date +"%s")
-## start coding
-
-
 bwlim=500
 rclone="$HOME/PROGRAMS/rclone"
 config="$HOME/Documents/rclone.conf"
 otheropt=" --checkers=20 --delete-before --stats=300s"
 bwlimit="  --bwlimit=${bwlim}k"
 
-
-"${rclone}" ${otheropt} ${bwlimit} --config "$config" --include "**/*.pdf"  sync "$HOME/CS_id/" "lapauththanasis:/CS_id_test"
-
-
-
+if [[ "$(hostname)" = "sagan" ]]; then 
+    echo "Upload all pdfs"
+    "${rclone}" ${otheropt} ${bwlimit} --config "$config" --include "**/*.pdf"  sync "$HOME/CS_id/" "lapauththanasis:/CS_id_test"
+else
+    echo "This runs only on sagan"
+fi
 
 exit 0 
