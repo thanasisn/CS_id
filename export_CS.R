7d24014249db8d858b9d5c20fc2d9dbc983db1b7
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-12-15 19:09:39 +0200

    Commit 2023-12-15 19:09

--- export_CS.R
+++ export_CS.R
@@ -2,6 +2,7 @@
 
 ####  Export CS data
 library(data.table)
+source("~/CODE/FUNCTIONS/R/data.R")
 
 filelist <- list.files(path = "~/DATA/Broad_Band/CS_id/",
                        pattern = ".*_id_.*_v14_2.*.Rds",
@@ -24,5 +25,28 @@ wecare <- grep("_11", wecare, invert = T, value = T)
 DATA[rowSums(DATA[, ..wecare], na.rm = T) == 0, TYPE := "Clear"]
 DATA[rowSums(DATA[, ..wecare], na.rm = T) != 0, TYPE := "Cloud"]
 
+## remove unused columns
+rm.cols.DT(DATA, "CSflag_*")
+rm.cols.DT(DATA, "QCF_*")
+rm.cols.DT(DATA, "QCv*")
+rm.cols.DT(DATA, "VIL*")
+rm.cols.DT(DATA, "CS_ref*")
+rm.cols.DT(DATA, "Dir*")
+rm.cols.DT(DATA, "Glo*")
+rm.cols.DT(DATA, "*temp*")
+rm.cols.DT(DATA, "GLBINC*")
+rm.cols.DT(DATA, "Clr*")
+rm.cols.DT(DATA, "*tmp_cr*")
+rm.cols.DT(DATA, "*ource")
+rm.cols.DT(DATA, "*_strict")
+rm.cols.DT(DATA, "wattHOR*")
+DATA[, Day      := NULL]
+DATA[, Elevat   := NULL]
+DATA[, RaylDIFF := NULL]
+DATA[, chp1TempCF := NULL ]
+DATA[, TSIextEARTH_comb := NULL ]
+DATA[, pressure := NULL ]
 
 
+write.csv(DATA, file = "Export_CS.csv", row.names = F)
+saveRDS(DATA,  file = "Export_CS.Rds")
